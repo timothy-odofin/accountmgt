8824530cdb025a64591595a8d8f6a2eb168b13c4
Odofin Oyejide <odofin@swipe.ng>
2022-08-08 16:18:56 +0100

    created project skeleton

---
+++ src/main/java/iobank/org/accountmgt/AccountmgtApplication.java
@@ -0,0 +1,13 @@
+package iobank.org.accountmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iobank/org/accountmgt/AccountmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package iobank.org.accountmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
