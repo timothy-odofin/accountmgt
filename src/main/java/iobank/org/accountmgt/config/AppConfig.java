b04deaa32e0b67babbd08ae471ee98d641b05af6
Odofin Oyejide <odofin@swipe.ng>
2022-08-10 11:43:24 +0100

    Added global configuration for cors and swagger resource

--- src/main/java/iobank/org/accountmgt/config/AppConfig.java
+++ src/main/java/iobank/org/accountmgt/config/AppConfig.java
@@ -1,4 +1,48 @@
 package iobank.org.accountmgt.config;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("iobank-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD",
+                        "GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "platform");
+
+    }
 }
