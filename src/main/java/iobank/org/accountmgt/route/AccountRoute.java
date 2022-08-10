f760c0b9118d7e80d6b4242dc82738d9bf0fcce1
Odofin Oyejide <odofin@swipe.ng>
2022-08-10 17:15:13 +0100

    Exposed AccountRoute service points

---
+++ src/main/java/iobank/org/accountmgt/route/AccountRoute.java
@@ -0,0 +1,43 @@
+package iobank.org.accountmgt.route;
+
+import iobank.org.accountmgt.model.request.AccountRequest;
+import iobank.org.accountmgt.model.request.BlockAccountRequest;
+import iobank.org.accountmgt.model.request.CustomerRequest;
+import iobank.org.accountmgt.model.response.ApiResponse;
+import iobank.org.accountmgt.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static iobank.org.accountmgt.utils.AccountEndpoints.*;
+
+@RestController
+@RequestMapping(CUSTOMER_ROOT)
+@RequiredArgsConstructor
+public class AccountRoute {
+    private final AccountService accountService;
+
+    @PostMapping(ADD_PATH)
+    ApiResponse addCustomer(@RequestBody CustomerRequest payload){
+        return accountService.addCustomer(payload);
+    }
+    @PostMapping(ADD_ACCOUNT_PATH)
+    ApiResponse addAccountToCustomer(@RequestBody AccountRequest payload){
+        return accountService.addAccountToCustomer(payload);
+    }
+    @PostMapping(SUSPEND_ACCOUNT_PATH)
+    ApiResponse suspendOrUnsuspendAccount(@RequestBody BlockAccountRequest payload){
+        return accountService.suspendOrUnsuspendAccount(payload);
+    }
+    @GetMapping(LIST_PATH)
+    ApiResponse listCustomer(){
+        return accountService.listCustomer();
+    }
+    @GetMapping(LIST_ACCOUNT_PATH)
+    ApiResponse listAccountsByCustomer(@RequestParam(value=PHONE_PARAM, required = false) String customerPhone){
+        return accountService.listAccountsByCustomer(customerPhone);
+    }
+    @GetMapping(ACCOUNT_PATH)
+    ApiResponse retrieveAccount(@RequestParam(ACCOUNT_PARAM) String accountNumber,@RequestParam(PHONE_PARAM) String customerPhone){
+        return accountService.retrieveAccount(accountNumber, customerPhone);
+    }
+}
---
+++ src/main/java/iobank/org/accountmgt/utils/AccountEndpoints.java
@@ -0,0 +1,15 @@
+package iobank.org.accountmgt.utils;
+
+public class AccountEndpoints {
+    public final static String CUSTOMER_ROOT="/customer";
+    public final static String ADD_PATH="/add";
+    public final static String LIST_PATH="/list";
+    public final static String CUSTOMER_PATH="/customer";
+    public final static String ACCOUNT_PATH="/account";
+    public final static String LIST_ACCOUNT_PATH=ACCOUNT_PATH+LIST_PATH;
+    public final static String ADD_ACCOUNT_PATH=ACCOUNT_PATH+ADD_PATH;
+    public final static String SUSPEND_ACCOUNT_PATH=ACCOUNT_PATH+"/suspend";
+    public final static String PHONE_PARAM="customerPhone";
+    public final static String ACCOUNT_PARAM="accountNumber";
+
+}
