5979ccbd7c258d3605a720aeff7ef60189c95a86
Odofin Oyejide <odofin@swipe.ng>
2022-08-10 11:44:13 +0100

    Added asynchronous annotation

--- src/main/java/iobank/org/accountmgt/config/AppConfig.java
+++ src/main/java/iobank/org/accountmgt/config/AppConfig.java
@@ -2,14 +2,20 @@ package iobank.org.accountmgt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableWebMvc
+@EnableAsync
+@EnableScheduling
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean(name = "asyncExecutor")
