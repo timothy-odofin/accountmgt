4e52f1ed7a9fcec3bfbf6b25dc9ddf2789762d45
Odofin Oyejide <odofin@swipe.ng>
2022-08-11 21:56:36 +0100

    Created RestMapper and delete unused testController

---
+++ src/test/java/iobank/org/accountmgt/model/RestMapper.java
@@ -0,0 +1,21 @@
+package iobank.org.accountmgt.model;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class RestMapper {
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+}
--- src/test/java/iobank/org/accountmgt/route/AccountRouteTest.java
+++ src/test/java/iobank/org/accountmgt/route/AccountRouteTest.java
@@ -1,20 +1,24 @@
 package iobank.org.accountmgt.route;
 
+import iobank.org.accountmgt.service.TransactionService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(AccountRoute.class)
 class AccountRouteTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TransactionService transactionService;
 
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void addCustomer() {
