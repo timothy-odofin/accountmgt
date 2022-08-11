daae089beea7fa243e3e93234dd1659e29e574c5
Odofin Oyejide <odofin@swipe.ng>
2022-08-11 21:26:53 +0100

    Created test data

--- src/test/java/iobank/org/accountmgt/model/DataUtils.java
+++ src/test/java/iobank/org/accountmgt/model/DataUtils.java
@@ -1,5 +1,33 @@
 package iobank.org.accountmgt.model;
 
-public class DataUtils {
+import iobank.org.accountmgt.model.request.CustomerRequest;
+import iobank.org.accountmgt.model.request.DepositRequest;
+import iobank.org.accountmgt.model.request.WithdrawalRequest;
 
+public class DataUtils {
+public static CustomerRequest customerData(){
+    return CustomerRequest.builder()
+            .contactAddress("No2 Rock City Avenue, Goshen Estate Abeokuta, Ogun State")
+            .email("odofin@swipe.ng")
+            .name("Odofin Timothy")
+            .phone("07065990878")
+            .build();
+}
+    public static DepositRequest depositData(){
+        return DepositRequest.builder()
+                .accountNumber("12344873")
+                .amount(2000.0)
+                .channel("Cash")
+                .narration("Testing deposit")
+                .build();
+    }
+
+    public static WithdrawalRequest withdrawalData(){
+        return WithdrawalRequest.builder()
+                .accountNumber("12344873")
+                .amount(2000.0)
+                .channel("Cash")
+                .narration("Testing deposit")
+                .build();
+    }
 }
