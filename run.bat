4e57cc5a03a3f110d361f40911f3e37c122a555e
Odofin Oyejide <odofin@swipe.ng>
2022-08-14 20:46:40 +0100

    Created run.bat for windows cmd

---
+++ run.bat
@@ -0,0 +1,22 @@
+
+@echo off
+SET DEVELOPMENT_HOME=C:\Users\JIDE\Desktop\codingchallenge\springboot_dockers
+
+cd %DEVELOPMENT_HOME%\account\
+
+REM Integration testing
+call mvn verify -Pfailsafe
+
+REM Building artifacts
+call mvn clean install
+
+REM shutdown or stop any previous docker-compose that is running
+call docker-compose down
+
+REM Build the docker image using the Dockerfile 
+call docker-compose build
+
+REM Start  docker container in an interactive mode
+call docker-compose up
+
+REM press ctl+c to stop  docker container in an interactive mode
